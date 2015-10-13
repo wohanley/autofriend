d7bb6d172c2af8eb199445200c94d8f98c583506
William O'Hanley <liam@wohanley.com>
2015-10-12 18:54:48 -0700

    move face recognition stuff into a class

---
+++ FaceRecognizer.py
@@ -0,0 +1,26 @@
+import cv2
+import np
+from os.path import isfile
+
+
+class FaceRecognizer():
+
+    recognizer_path = 'resources/recognizer'
+
+    def __init__(self):
+
+        self.recognizer = cv2.createLBPHFaceRecognizer()
+
+        if isfile(self.recognizer_path):
+            self.recognizer.load(self.recognizer_path)
+
+    def update(self, updates):
+
+        self.recognizer.update(
+            np.array([image for (image, _) in updates]),
+            np.array([label for (_, label) in updates]))
+
+        self.recognizer.save(self.recognizer_path)
+
+    def recognize_face(self, region):
+        return self.recognizer.predict(region)
