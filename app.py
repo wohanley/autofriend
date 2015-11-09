0705bf04c81c57f7877791f4d281cb6c2481d174
William O'Hanley <liam@wohanley.com>
2015-11-08 20:43:11 -0800

    don't use WSGI

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os
+import twitter
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+twitter.Autofriend().run()
