e777f14a788ae5ba04ae606535034fc09e2fd0bd
William O'Hanley <liam@wohanley.com>
2015-11-08 20:20:28 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Autofriend',
+      version='1.0',
+      description="a Twitter bot that compliments selfies",
+      author="William O'Hanley",
+      author_email="me@wohanley.com",
+      url='http://www.python.org/sigs/distutils-sig/'
+      # install_requires=['Django>=1.3'],
+      )
